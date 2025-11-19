72525bb93b0f9d7fb514e4f24a9006305dc1c217
Leonhard Suharjo <leonhard.suharjo@gmail.com>
2025-11-19 10:28:22 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-''' testing git cloud push and commit '''
-print('Hello, this is my capstone project, im leon')
-
 '''' main.py file == py file that will host main AI agent module'''
+
+''' program structure in steps recaped: 
+1. Install dependencies.
+2. Configure Gemini API key.
+3. Import ADK and Gemini components.
+4. Build and define agents.
+5. Set agent instructions and tools.
+6. Run agent pipeline in cell order.
+'''
